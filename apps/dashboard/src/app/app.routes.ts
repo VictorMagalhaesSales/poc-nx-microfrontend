f6c70daa6b88443c745074c296fa85b3b55e9017
Victor Magalhaes Sales <victormagalhaessales@gmail.com>
2025-09-22 18:27:08 -0300

    Loading remote modules defitions from a http requisition

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Route } from '@angular/router';
 import { App } from './app';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
-    loadChildren: () => import('login/Routes').then((m) => m!.remoteRoutes),
+    loadChildren: () => loadRemoteModule('login', './Routes').then((m) => m.remoteRoutes),
   },
   {
     path: '',
