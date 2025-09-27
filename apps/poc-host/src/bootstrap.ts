242988eeb2e530d3599b8d6beff402a93319cb9b
Victor Magalhães <victormagalhaessales@gmail.com>
2025-09-27 11:10:21 -0300

    Creating poc-host application

---
+++ apps/poc-host/src/bootstrap.ts
@@ -0,0 +1,13 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
+
+bootstrapApplication(App, {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+  ],
+}).catch((err) => console.error(err));
